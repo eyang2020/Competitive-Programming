4ac234fad0329cac78e3aec0d8a41ff62525efbd
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2021-08-08 18:20:04 -0500

    Create SubarraySumsI.cpp

---
+++ CSES/C++/Sorting-and-Searching/SubarraySumsI.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	ll k;
+	cin >> n >> k;
+	ll sum = 0;
+	int res = 0;
+	map<ll, int> mp;
+	for(int i=0;i<n;i++) {
+		ll x;
+		cin >> x;
+		sum += x;
+		if(sum == k) res++;
+		res += mp[sum - k];
+		mp[sum]++;
+	}
+	cout << res << endl;
+}
