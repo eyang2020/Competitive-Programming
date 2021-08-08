db539961477eaeafb401aa4632d0ae3bca0f420a
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2021-08-08 17:49:27 -0500

    Create NearestSmallerValues.cpp

---
+++ CSES/C++/Sorting-and-Searching/NearestSmallerValues.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	stack<pair<int, int>> stk;
+	for(int i=0;i<n;i++) {
+		int x;
+		cin >> x;
+		while(!stk.empty() && stk.top().first >= x) stk.pop();
+		cout << (stk.empty() ? 0 : stk.top().second) << ' ';
+		stk.push({x, i + 1});
+	}
+}
