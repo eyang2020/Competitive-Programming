402202432a0550daa8236c639fc8653c4e800c76
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2021-08-10 13:09:19 -0500

    Create DiceCombinations.cpp

---
+++ CSES/C++/Dynamic-Programming/DiceCombinations.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+const ll MOD = 1e9 + 7;
+
+int main() {
+	int n;
+	cin >> n;
+	// let dp[i] represent the number of ways sum x can be made using numbers 1-6
+	vector<ll> dp(n + 1);
+	dp[0] = 1;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=6&&i-j>=0;j++) {
+			dp[i] = (dp[i] + dp[i - j]) % MOD;
+		}
+	}
+	cout << dp[n] << endl;
+}
