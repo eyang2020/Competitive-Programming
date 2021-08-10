65b9e2a6c08390987e34b411366a15896fcacf5a
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2021-08-10 12:41:32 -0500

    Create SubarrayDivisibility.cpp

---
+++ CSES/C++/Sorting-and-Searching/SubarrayDivisibility.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+int main() {
+	int n;
+	cin >> n;
+	ll prefix = 0, res = 0;
+	map<ll, int> cnt;
+	cnt[0] = 1;
+	for(int i=0;i<n;i++) {
+		ll x;
+		cin >> x;
+		prefix = (((prefix + x) % n) + n) % n;
+		res += cnt[prefix]++;
+	}
+	cout << res << endl;
+}
