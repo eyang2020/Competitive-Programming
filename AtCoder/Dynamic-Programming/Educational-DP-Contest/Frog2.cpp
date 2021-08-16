7bb22859998bb646daf4b54bb11b31268b45abd2
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2021-08-16 15:38:15 -0400

    Update Frog2.cpp

--- AtCoder/Dynamic-Programming/Educational-DP-Contest/Frog2.cpp
+++ AtCoder/Dynamic-Programming/Educational-DP-Contest/Frog2.cpp
@@ -10,7 +10,7 @@ const ll MOD = 1e9 + 7;
 int main() {
     int n, k;
     cin >> n >> k;
-    // let dp[i] represent the minimum cost to reach stone i from stone 0
+    // let dp[i] represent the minimum cost to jump from stone 0 to stone i
     vector<int> h(n), dp(n, 1e9);
     for(int i=0;i<n;i++) cin >> h[i];
     dp[0] = 0;
