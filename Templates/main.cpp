b66b5e890c9b99beb9d0c0532dbdce9115887bc7
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2021-08-24 08:56:27 -0400

    Create main.cpp

---
+++ Templates/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+const ll MOD = 1e9 + 7;
+
+int main() {
+    IOS
+    
+    return 0;
+}
