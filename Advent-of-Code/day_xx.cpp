3779099e411a9f00e42d5cf686e20e810547b4a8
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2022-11-30 20:49:34 -0500

    Create day_xx.cpp

---
+++ Advent-of-Code/day_xx.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+void part1() {
+
+}
+
+void part2() {
+
+}
+
+int main() {
+	part1();
+	part2();
+}
