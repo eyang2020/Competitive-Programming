89b9bfab81a2cf8414e655fe7cbe207945eb32a2
Evan Yang <28735495+eyang2020@users.noreply.github.com>
2024-11-03 17:21:46 -0600

    Update day_xx.cpp

--- Advent-of-Code/day_xx.cpp
+++ Advent-of-Code/day_xx.cpp
@@ -1,27 +1,45 @@
 #include <bits/stdc++.h>
-
+    
 using namespace std;
-
+    
 typedef long long ll;
 typedef long double ld;
 #define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
+const int N = 1e5;
+const ll MOD = 1e9 + 7;
 const int INF = 1e9;
+
 const int dx[4] = {0, 1, 0, -1};
 const int dy[4] = {1, 0, -1, 0};
 
-vector<string> split(string s, string delim) {
-    int pos;
-    string token;
+const int dxWithDiag[8] = {0, 1, 0, -1, 1, -1, 1, -1};
+const int dyWithDiag[8] = {1, 0, -1, 0, -1, 1, 1, -1};
+
+ll modpow(ll x, ll y) {
+    ll res = 1;
+    x %= MOD;
+    if(x == 0) return 0;
+    while(y > 0) {
+        if(y & 1) {
+            res = (res * x) % MOD;
+        }
+        y >>= 1;
+        x = (x * x) % MOD;
+    }
+    return res;
+}
+
+vector<string> tokenize(string s, string delim) {
     vector<string> tokens;
-    while((pos = s.find(delim)) != string::npos) {
-        token = s.substr(0, pos);
-        s.erase(0, pos + delim.length());
+    int idx = 0, n = delim.length();
+    string token;
+    while((idx = s.find(delim)) != string::npos) {
+        token = s.substr(0, idx);
         tokens.push_back(token);
+        s.erase(0, idx + n);
     }
-    if (s.length() > 0) {
-        tokens.push_back(s);
-    }
+    tokens.push_back(s);
     return tokens;
 }
 
@@ -34,7 +52,8 @@ void part2() {
 }
 
 int main() {
-	IOS
-	part1();
-	part2();
+    IOS
+    part1();
+    part2();
+    return 0;
 }
